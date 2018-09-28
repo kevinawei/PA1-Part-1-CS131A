02ac96ad1a5f8f158647802a7f819302e0716907
kevinawei <kevinawei@gmail.com>
2018-09-28 02:36:47 -0400

    Add files via upload

---
+++ UniqFilter.java
@@ -0,0 +1,11 @@
+package cs131.pa1.filter;
+
+import cs131.pa1.filter.sequential.SequentialFilter;
+
+public class UniqFilter extends SequentialFilter {
+	
+	protected String processLine(String line) {
+		return null;
+	}
+	
+}
