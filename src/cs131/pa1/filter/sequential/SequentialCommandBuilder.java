5ea7027799935b4c9683d8f134243e84cc564207
kevinawei <kevinawei@gmail.com>
2018-09-28 18:14:34 -0400

    Update SequentialCommandBuilder.java

--- src/cs131/pa1/filter/sequential/SequentialCommandBuilder.java
+++ src/cs131/pa1/filter/sequential/SequentialCommandBuilder.java
@@ -1,11 +1,28 @@
 package cs131.pa1.filter.sequential;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class SequentialCommandBuilder {
+	private static final List<String> C = Arrays.asList("grep", "cat", "wc", "uniq", "exit", "pwd", "ls", "cd", ">");
+	private static List<SequentialFilter> fList;
+	public static final String PIPE = "|";
 	
 	public static List<SequentialFilter> createFiltersFromCommand(String command){
-		return null;
+		
+		if (command.contains(PIPE)){
+			String[] commands = command.split(PIPE);
+			
+			for(int i = 0; i < commands.length ;i++) {
+				fList.add(constructFilterFromSubCommand(commands[i]));
+			}
+			linkFilters(fList);
+		} 
+		else {
+			fList.add(constructFilterFromSubCommand(command));
+		}
+		
+		return fList;
 	}
 	
 	private static SequentialFilter determineFinalFilter(String command){
@@ -17,7 +34,9 @@ public class SequentialCommandBuilder {
 	}
 	
 	private static SequentialFilter constructFilterFromSubCommand(String subCommand){
+		
 		return null;
+		
 	}
 
 	private static boolean linkFilters(List<SequentialFilter> filters){
