78be9cbfe1d2137274d6e49f5cf8ea43e4f7c397
kevinawei <kevinawei@gmail.com>
2018-09-29 02:41:37 -0400

    Update UniqFilter.java

--- src/cs131/pa1/command/textmanip/UniqFilter.java
+++ src/cs131/pa1/command/textmanip/UniqFilter.java
@@ -1,11 +1,20 @@
-package cs131.pa1.filter;
+package cs131.pa1.command.textmanip;
 
 import cs131.pa1.filter.sequential.SequentialFilter;
+import java.util.HashSet;
 
 public class UniqFilter extends SequentialFilter {
 	
+	private HashSet<String> lines;
+
 	protected String processLine(String line) {
-		return null;
+		if (lines.contains(line)) {
+			return null;
+		}
+		else {
+			lines.add(line);
+			return line;
+		}
 	}
 	
 }
