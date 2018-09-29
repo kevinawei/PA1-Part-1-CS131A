63a62683227c8e4cbc6c7555c989015913d5c820
kevinawei <kevinawei@gmail.com>
2018-09-29 02:48:55 -0400

    Update SequentialREPL.java
    Added shouldExit boolean for ExitFilter

--- src/cs131/pa1/filter/sequential/SequentialREPL.java
+++ src/cs131/pa1/filter/sequential/SequentialREPL.java
@@ -6,8 +6,10 @@ import cs131.pa1.filter.Message;
 public class SequentialREPL {
 
 	static String currentWorkingDirectory;
+	public static boolean shouldExit;
 	
 	public static void main(String[] args){
+		shouldExit = false;
 		System.out.println(Message.WELCOME.toString());
 		do {
 			System.out.print(Message.NEWCOMMAND.toString());
@@ -18,7 +20,7 @@ public class SequentialREPL {
 				input.add(S);
 			}
 			//Send input to somewhere it's a linked list of strings
-		} while () ;
+		} while (!shouldhExit) ;
 		
 	}
 
