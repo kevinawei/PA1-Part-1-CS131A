d5eac4f86dbce08b855ffb8d72ef9f9a21e75b52
kevinawei <kevinawei@gmail.com>
2018-09-29 02:42:53 -0400

    Update GrepFilter.java

--- src/cs131/pa1/command/textmanip/GrepFilter.java
+++ src/cs131/pa1/command/textmanip/GrepFilter.java
@@ -1,10 +1,16 @@
-package cs131.pa1.filter;
+package cs131.pa1.command.textmanip;
+
 
 import cs131.pa1.filter.sequential.SequentialFilter;
 
 public class GrepFilter extends SequentialFilter {
+	public String search;
 	
+	@Override
 	protected String processLine(String line) {
+		if (line.contains(search)) {
+			return line;
+		}
 		return null;
 	}
 }
