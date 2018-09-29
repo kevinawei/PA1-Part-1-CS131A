8f63bd129b02b6869d831910e18a27cf78516f6e
kevinawei <kevinawei@gmail.com>
2018-09-29 01:13:37 -0400

    Update SequentialCommandBuilder.java
    Things Implemented:
    -Checks that a given input is a valid command
    -Separates inputs by "|"
    -Determines which command is being input and creates an instance of that filter
    -Links the filters together in order

--- src/cs131/pa1/filter/sequential/SequentialCommandBuilder.java
+++ src/cs131/pa1/filter/sequential/SequentialCommandBuilder.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import cs131.pa1.filter.Message;
 import cs131.pa1.command.textmanip.*;
+import cs131.pa1.command.general.*;
 
 public class SequentialCommandBuilder {
 	private static final List<String> C = Arrays.asList("grep", "cat", "wc", "uniq", "exit", "pwd", "ls", "cd", ">");
@@ -40,16 +41,19 @@ public class SequentialCommandBuilder {
 		
 		if(!C.parallelStream().anyMatch(subCommand::contains)) {
 			System.out.println("Message.COMMAND_NOT_FOUND");
-		return null;
+			return null;
 		}
 		if(subCommand.contains("pwd")) {
-			
+			PwdFilter p = new PwdFilter();
+			return p;
 		}
 		if(subCommand.contains("ls")) {
-			
+			LsFilter l = new LsFilter();
+			return l;
 		}
 		if(subCommand.contains("cd")) {
-			
+			CdFilter cd = new CdFilter();
+			return cd;
 		}
 		if(subCommand.contains("cat")) {
 			CatFilter c = new CatFilter();
@@ -71,16 +75,26 @@ public class SequentialCommandBuilder {
 			return wc;
 		}
 		if(subCommand.contains(">")) {
-			
+			ImplyingCarrotFilter icf = new ImplyingCarrotFilter();
+			return icf;
 		}
 		if(subCommand.contains("exit")) {
-			
+			ExitFilter e = new ExitFilter();
+			return e;
 		}
 		
 		
 	}
 
 	private static boolean linkFilters(List<SequentialFilter> filters){
+		for (int i =0; i< filters.size(); i++) {
+			SequentialFilter first = filters.get(i);
+			SequentialFilter second = null;
+			if (filters.size() > i+1) {
+				second = filters.get(i+1);
+			}
+			first.setNextFilter(second);
+		}
 		return false;
 	}
 }
