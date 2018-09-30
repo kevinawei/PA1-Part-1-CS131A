095c6ddfcc8ccc5d28f9d2098899e0c8ed4dcbc4
kevinawei <kevinawei@gmail.com>
2018-09-29 23:36:45 -0400

    Added error messages

--- src/cs131/pa1/filter/sequential/SequentialCommandBuilder.java
+++ src/cs131/pa1/filter/sequential/SequentialCommandBuilder.java
@@ -9,6 +9,7 @@ import cs131.pa1.command.general.*;
 
 public class SequentialCommandBuilder {
 	private static final List<String> C = Arrays.asList("grep", "cat", "wc", "uniq", "exit", "pwd", "ls", "cd", ">");
+	private static final List<String> T = Arrays.asList("grep", "uniq", ">", "wc");
 	private static List<SequentialFilter> fList;
 	public static final String PIPE = "|";
 	
@@ -20,13 +21,18 @@ public class SequentialCommandBuilder {
 			for(int i = 0; i < commands.length ;i++) {
 				fList.add(constructFilterFromSubCommand(commands[i]));
 			}
-			linkFilters(fList);
+			if (!linkFilters(fList)) {
+				SequentialREPL.shouldExit = true;
+				return null;
+			}
+			return fList;
 		} 
 		else {
 			fList.add(constructFilterFromSubCommand(command));
+			return fList;
 		}
 		
-		return fList;
+	
 	}
 	
 	private static SequentialFilter determineFinalFilter(String command){
@@ -40,7 +46,7 @@ public class SequentialCommandBuilder {
 	private static SequentialFilter constructFilterFromSubCommand(String subCommand){
 		
 		if(!C.parallelStream().anyMatch(subCommand::contains)) {
-			System.out.println("Message.COMMAND_NOT_FOUND");
+			System.out.println(Message.COMMAND_NOT_FOUND.toString());
 			return null;
 		}
 		if(subCommand.contains("pwd")) {
@@ -52,15 +58,31 @@ public class SequentialCommandBuilder {
 			return l;
 		}
 		if(subCommand.contains("cd")) {
-			CdFilter cd = new CdFilter();
+			String[] parts = subCommand.split(" ");
+			if (parts.length == 1) {
+				System.out.println(Message.REQUIRES_PARAMETER.toString());
+				return null;
+			}
+			CdFilter cd = new CdFilter(parts[1]);
 			return cd;
 		}
 		if(subCommand.contains("cat")) {
-			CatFilter c = new CatFilter();
+			String[] parts = subCommand.split(" ");
+			if (parts.length == 1) {
+				System.out.println(Message.REQUIRES_PARAMETER.toString());
+				return null;
+			}
+			else {
+			CatFilter c = new CatFilter(parts[1]);
 			return c;
+			}
 		}
 		if(subCommand.contains("grep")) {
 			String[] parts = subCommand.split(" ");
+			if (parts.length == 1) {
+				System.out.println(Message.REQUIRES_PARAMETER.toString());
+				return null;
+			}
 			GrepFilter g = new GrepFilter();
 			g.search = parts[1];
 			return g;
@@ -71,7 +93,7 @@ public class SequentialCommandBuilder {
 			return u;
 		}
 		if(subCommand.contains("wc")) {
-			WcFilter wc = new WCFilter();
+			WCFilter wc = new WCFilter();
 			return wc;
 		}
 		if(subCommand.contains(">")) {
@@ -87,6 +109,10 @@ public class SequentialCommandBuilder {
 	}
 
 	private static boolean linkFilters(List<SequentialFilter> filters){
+		if (T.contains(filters.get(0).type)) {
+			System.out.println(Message.REQUIRES_INPUT.toString());
+			return false;
+		}
 		for (int i =0; i< filters.size(); i++) {
 			SequentialFilter first = filters.get(i);
 			SequentialFilter second = null;
@@ -95,6 +121,6 @@ public class SequentialCommandBuilder {
 			}
 			first.setNextFilter(second);
 		}
-		return false;
+		return true;
 	}
 }
